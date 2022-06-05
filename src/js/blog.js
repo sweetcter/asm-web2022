ec21ca4457e2e4dcd652ba8e44a1a7b67e7c1a54
sweetcter <daylasep0175@gmail.com>
2022-06-05 15:45:22 +0700

    update

---
+++ src/js/blog.js
@@ -0,0 +1,56 @@
+const blog = [
+    {
+
+        title: 'UI Interactions of the week',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        date: '12 Feb 2019',
+        short_description: 'Express, Handlebars',
+    },
+    {
+
+        title: 'UI Interactions of the week',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        date: '12 Feb 2019',
+        short_description: 'Express, Handlebars',
+    },
+    {
+
+        title: 'UI Interactions of the week',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        date: '12 Feb 2019',
+        short_description: 'Express, Handlebars',
+    },
+    {
+
+        title: 'UI Interactions of the week',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        date: '12 Feb 2019',
+        short_description: 'Express, Handlebars',
+    },
+];
+    function listBlog (blog) {
+        if(!Array.isArray(blog) || blog.length==0) return false
+            let result='';
+        for(let i =0; i<blog.length;i++) {
+            const blogs = blog[i];
+            result += `
+            <div class=" border-b border-[#E0E0E0] flex mt-8">
+            <div class="basis-[65%] bg-white ">
+         <h3 class="font-bold text-[32px] leading-[38px ">${blogs.title}</h3>
+         <span class=" my-2 mr-2 inline-block text-lg font-normal relative after:content-[''] after:border-solid after:w-[1px] after:h-[24px] 
+        after:bg-black after:block after:absolute after:top-[50%] after:translate-y-[-50%] after:right-[-25px]  ">${blogs.date}</span>
+         <span class=" my-2 mx-9 inline-block text-lg font-normal text-[#8695A4]">${blogs.short_description}</span>
+         <p class=" font-normal text-base mb-8 text-left">${blogs.description}</p>
+         </div>
+         </div>`
+        }
+        return result;
+     }
+     function render (elementId, content ) {
+         const element = document.getElementById(elementId);
+         if(element){
+             element.innerHTML=content;
+         }
+     }
+     render('blog', listBlog(blog));
+    
