abf5179e3107cb3fe473aecc8683778897b37743
sweetcter <105471517+sweetcter@users.noreply.github.com>
2023-06-08 22:19:15 +0700

    Update blog.js

--- src/js/blog.js
+++ src/js/blog.js
@@ -4,28 +4,28 @@ const blog = [
         title: 'UI Interactions of the week',
         description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         date: '12 Feb 2019',
-        short_description: 'Express, Handlebars',
+        short_description: 'http://hackerlord.id.vn/',
     },
     {
 
         title: 'UI Interactions of the week',
         description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         date: '12 Feb 2019',
-        short_description: 'Express, Handlebars',
+        short_description: 'http://hackerlord.id.vn/',
     },
     {
 
         title: 'UI Interactions of the week',
         description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         date: '12 Feb 2019',
-        short_description: 'Express, Handlebars',
+        short_description: 'http://hackerlord.id.vn/',
     },
     {
 
         title: 'UI Interactions of the week',
         description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
         date: '12 Feb 2019',
-        short_description: 'Express, Handlebars',
+        short_description: 'http://hackerlord.id.vn/',
     },
 ];
     function listBlog (blog) {
